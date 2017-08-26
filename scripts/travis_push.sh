788bbf1db8013425dce8dc986b98184a8388bb00
Programador Pobre <programadorpobre@protonmail.com>
2017-08-26 22:35:32 +0000

    fix

--- scripts/travis_push.sh
+++ scripts/travis_push.sh
@@ -8,7 +8,7 @@ git config --global user.name "Travis CI"
 git add _includes/feed-entries.html
 git commit --message "[travis] #$TRAVIS_BUILD_NUMBER"
 
-git remote add github https://${GITHUB_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+git remote add github https://${GITHUB_TOKEN}@github.com/programadorpobre/programadorpobre.github.io > /dev/null 2>&1
 git push --quiet --set-upstream github master > /dev/null 2>&1
 
 echo "./scripts/travis_push.sh finished"
