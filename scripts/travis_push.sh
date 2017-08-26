2b8c7c4afb71de1ebe751dd0dfb76ec0151c0a53
Programador Pobre <programadorpobre@protonmail.com>
2017-08-26 17:41:07 +0000

    log messages

--- scripts/travis_push.sh
+++ scripts/travis_push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "./scripts/travis_push.sh started"
+
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
@@ -9,3 +11,4 @@ git commit --message "[travis] #$TRAVIS_BUILD_NUMBER"
 git remote add github https://${GITHUB_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
 git push --quiet --set-upstream github master > /dev/null 2>&1
 
+echo "./scripts/travis_push.sh finished"
