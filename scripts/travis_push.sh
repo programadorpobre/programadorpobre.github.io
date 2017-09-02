14a60f143d6ccca354b2dd373cc4c47e624aafc3
Programador Pobre <programadorpobre@protonmail.com>
2017-09-02 16:52:00 +0000

    travis test

--- scripts/travis_push.sh
+++ scripts/travis_push.sh
@@ -5,10 +5,12 @@ echo "./scripts/travis_push.sh started"
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
+git checkout master
+
 git add _includes/feed-entries.html
 git commit --message "[travis] #$TRAVIS_BUILD_NUMBER"
 
-git remote add github-origin https://${GITHUB_TOKEN}@github.com/programadorpobre/programadorpobre.github.io > /dev/null 2>&1
-git push --quiet --set-upstream github-origin master > /dev/null 2>&1
+git remote add github-origin https://${GITHUB_TOKEN}@github.com/programadorpobre/programadorpobre.github.io
+git push -u github-origin master
 
 echo "./scripts/travis_push.sh finished"
